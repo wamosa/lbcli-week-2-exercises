006d5590f33d9b9811de84a49363dd5f3dd2183a
Salha Benazir <105310587+wamosa@users.noreply.github.com>
2025-03-26 23:29:40 +0300

    Update 03.sh

--- submission/03.sh
+++ submission/03.sh
@@ -1,3 +1,5 @@
 # Created a SegWit address.
 # Add funds to the address.
 # Return only the Address
+
+bitcoin-cli -rpcwallet="your_wallet_name" getnewaddress "my_segwit_address" "bech32" | jq -r '.'
